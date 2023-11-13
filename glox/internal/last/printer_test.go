f50366a814940b300544e70f1f9498c7e09a599d
Toby Law <toby@tzcl.me>
2023-11-14 09:42:34 +1100

    glox/pretty-printer

---
+++ glox/internal/last/printer_test.go
@@ -0,0 +1,24 @@
+package last_test
+
+import (
+	"testing"
+
+	"github.com/hexops/autogold/v2"
+	"github.com/tzcl/lox/glox/internal/last"
+	"github.com/tzcl/lox/glox/internal/ltoken"
+)
+
+func TestPrint(t *testing.T) {
+	expr := last.BinaryExpr{
+		Operator: ltoken.Token{Type: ltoken.Star, Lexeme: "*", Literal: nil, Line: 1},
+		Left: last.UnaryExpr{
+			Operator: ltoken.Token{Type: ltoken.Minus, Lexeme: "-", Literal: nil, Line: 1},
+			Expr:     last.LiteralExpr{123},
+		},
+		Right: last.GroupingExpr{
+			Expr: last.LiteralExpr{45.67},
+		},
+	}
+
+	autogold.Expect("(* (- 123) (45.67))").Equal(t, last.Print(expr))
+}
